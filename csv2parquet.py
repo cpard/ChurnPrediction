65bff1a5e826b44cfc996a30c4604457cd45d97e
cpard <costas.pardalis@gmail.com>
2021-08-13 12:25:12 -0700

    initial commit

---
+++ csv2parquet.py
@@ -0,0 +1,33 @@
+import argparse
+import pyarrow as pa
+from pyarrow import csv
+from pyarrow.csv import ConvertOptions
+import pyarrow.parquet as pq
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert CSV to Parquet')
+    parser.add_argument('-c', '--csv', help="the path to the csv file to be converted.")
+    return parser.parse_args()
+
+
+def convert(csv_file):
+    out_file = str(csv_file).split('.')[0] + '.parquet'
+    convert_opts = ConvertOptions(timestamp_parsers=['%Y-%m-%d'], column_types={'date': pa.date32()})
+
+    reader = csv.open_csv(csv_file, convert_options=convert_opts)
+
+    writer = pq.ParquetWriter('example2.parquet', reader.schema)
+
+    table = pa.Table.from_batches(reader, reader.schema)
+    writer.write_table(table)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if args.csv is None:
+        print("Where's the CSV file dude")
+        quit()
+
+    convert(args.csv)
