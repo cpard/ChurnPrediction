94795f069fcb7385feb6ef46410561dc14dac16e
cpard <costas.pardalis@gmail.com>
2021-08-15 18:24:10 -0700

    train the final churn prediction model
    it has some ugly hacks because of the limited time functions support duckdb currently has.

--- train_model.py
+++ train_model.py
@@ -1,7 +1,13 @@
 import argparse
 import duckdb
+import pandas
+
+from lifetimes.fitters.pareto_nbd_fitter import ParetoNBDFitter
+from lifetimes import BetaGeoFitter
 
 con = duckdb.connect(database=':memory:', read_only=False)
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.max_columns', None)
 
 
 def parse_args():
@@ -23,12 +29,35 @@ def create_features(db):
     con.execute(initial_view)
     con.execute(first_iteration)
     con.execute(features)
-    con.execute("select * from features")
 
-    l = con.df()
-    l['t'] = l['t'].astype('timedelta64[D]')
-    l['recency'] = l['recency'].astype('timedelta64[D]')
-    print(l)
+    # we need todo this because for some unknown reason the intervals that come back into the dataframe are all wrong,
+    # this is probably a bug with the duckdb implementation. Very ungly hack to turn an interval into a count of days,
+    # this is because of the limitations of duckdb in terms of date functions.
+
+    con.execute(
+        "select user_id, freq, "
+        "extract('year' from recency) as recency_year, "
+        "extract('month' from recency) as recency_month, "
+        "extract('day' from recency) as recency_day, "
+        "extract('year' from t) as t_year, "
+        "extract('month' from t) as t_month, "
+        "extract('day' from t) as t_day "
+        "from features")
+
+    db_result = con.df()
+
+    # calculate number of days from years, months and days. We consider 30.436875 days in a month on an average.
+    db_result['t'] = round(
+        db_result['t_year'] * 365 + db_result['t_month'] * 30.436875 + db_result['t_day'], 0)
+    db_result['recency'] = round(
+        db_result['recency_year'] * 365 + db_result['recency_month'] * 30.436875 + db_result['recency_day'], 0)
+
+    db_result = db_result.drop(['recency_year', 'recency_day', 'recency_month', 't_year', 't_month', 't_day'], axis=1)
+
+    filtered_freqs = db_result[db_result['freq'] > 0]
+    model = BetaGeoFitter()
+    model.fit(filtered_freqs['freq'], filtered_freqs['recency'], filtered_freqs['t'])
+    print(model.summary)
 
 
 if __name__ == '__main__':
